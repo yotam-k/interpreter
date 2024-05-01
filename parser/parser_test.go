a369ad9a6e539870edefc8d751786c4e57d4776d
Yotam Kanny <yotamkanny3@gmail.com>
2024-05-01 10:41:31 -0600

    Implement parsing of return statements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -6,11 +6,11 @@ import (
 	"testing"
 )
 
-func TestLetStatements(t *testing.T) {
+func TestReturnStatements(t *testing.T) {
 	input := `
-let x 5;
-let = 10;
-let 838383;
+return 5;
+return 10;
+return 993322;
 `
 
 	l := lexer.New(input)
@@ -20,7 +20,39 @@ let 838383;
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
+	checkParserErrors(t, p)
 
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("s not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return'. got=%q", returnStmt.TokenLiteral())
+		}
+	}
+}
+
+func TestLetStatements(t *testing.T) {
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
 	checkParserErrors(t, p)
 
 	if len(program.Statements) != 3 {
