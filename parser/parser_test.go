ad2eaec6251df1c5cc21834840cb4030c4e1ae93
Yotam Kanny <yotamkanny3@gmail.com>
2024-05-03 13:25:53 -0600

    Implement support for parsing identifiers and operator precedence

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -6,6 +6,39 @@ import (
 	"testing"
 )
 
+func TestIdentifierExpression(t *testing.T) {
+	input := "foobar;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d",
+			len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Errorf("program.Statements[0] is not *ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	if !ok {
+		t.Fatalf("exp not *ast.Identifier. got=%T", stmt.Expression)
+	}
+	if ident.Value != "foobar" {
+		t.Errorf("ident.Value not %s. got=%s", "foobar", ident.Value)
+	}
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", "foobar",
+			ident.TokenLiteral())
+	}
+
+}
+
 func TestReturnStatements(t *testing.T) {
 	input := `
 return 5;
@@ -15,11 +48,7 @@ return 993322;
 
 	l := lexer.New(input)
 	p := New(l)
-
 	program := p.ParseProgram()
-	if program == nil {
-		t.Fatalf("ParseProgram() returned nil")
-	}
 	checkParserErrors(t, p)
 
 	if len(program.Statements) != 3 {
@@ -48,11 +77,7 @@ let foobar = 838383;
 
 	l := lexer.New(input)
 	p := New(l)
-
 	program := p.ParseProgram()
-	if program == nil {
-		t.Fatalf("ParseProgram() returned nil")
-	}
 	checkParserErrors(t, p)
 
 	if len(program.Statements) != 3 {
