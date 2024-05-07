7862509172d1093f22a828d42d5785e21d509e58
Yotam Kanny <yotamkanny3@gmail.com>
2024-05-06 22:25:32 -0600

    Fix up REPL and bug in let/return parsing

--- parser/parser.go
+++ parser/parser.go
@@ -178,7 +178,7 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
 	stmt.Value = p.parseExpression(LOWEST)
 
-	for !p.curTokenIs(token.SEMICOLON) {
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -192,7 +192,7 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	stmt.ReturnValue = p.parseExpression(LOWEST)
 
-	for !p.curTokenIs(token.SEMICOLON) {
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
