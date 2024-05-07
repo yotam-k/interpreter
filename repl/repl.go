64eda25787491177a71e675edd9ee4cad5d7953e
Yotam Kanny <yotamkanny3@gmail.com>
2024-05-07 12:44:59 -0600

    Add evaluation to REPL

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"interpreter/evaluator"
 	"interpreter/lexer"
 	"interpreter/parser"
 	"io"
@@ -43,8 +44,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+
 	}
 }
 
